1c982f2129c4d3595c7c2a5a6f15023e05adc7ec
Shachar Alon <shachar.alon@bystronic.com>
2022-06-24 10:35:01 +0200

    Added Project

---
+++ Excel VBA/Excel VBA/Model/FileinformationDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_VBA.Model
+{
+	public class FileinformationDto
+	{
+		public string Kalkulationsstruktur { get; set; }
+		public string Preiseinheit { get; set; }
+		public string ME { get; set; }
+		public string Preis { get; set; }
+		public string Kalkulationslosgroesse { get; set; }
+		public string Menge { get; set; }
+		public string BWME { get; set; }
+		public string WertGesamt { get; set; }
+		public string Waehrung { get; set; }
+		public string Ressource { get; set; }
+		public string Fehlerstatus { get; set; }
+		public string PreisstrategieText { get; set; }
+		public string KostenarteText { get; set; }
+		public string KennzeichenBaugruppe { get; set; }
+	}
+}
---
+++ Excel VBA/Excel VBA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Excel_VBA.Controller;
+using Excel_VBA.Model;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Excel_VBA
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// C:\Users\chash\Documents\GitHub\Excel VBA\Excel VBA\LaserschneidmaschineExcelTest.csv
+
+			ComparisonController comparisonController = new ComparisonController();
+			comparisonController.compare();
+		}
+	}
+}
